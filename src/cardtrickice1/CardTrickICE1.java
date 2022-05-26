52b01de0f761489f126b9b8d049048121af238ae
Pizaan <tadiwala@sheridancollege.ca>
2022-05-26 18:27:28 -0400

    Finishing up the ICE.

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -1,33 +1,46 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    Name: Pizaan Tadiwala
+*/
+
 package cardtrickice1;
 
-/** step1 : generate 7 random cards and store in array - how
+import java.util.Scanner;
+
+/*
+ * step 1: generate 7 random cards and store in an array 
  * step 2: take any card input from user suit,number
  * step 3: user card is in  the array 'card is found'
- *
- * @author sivagamasrinivasan,May 23rd
  */
 public class CardTrickICE1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7]; //Array of object
-        for( int i=0;i<magicHand.length;i++)
-        {
-            Card c1 = new Card();
-            c1.setValue(2);//use a method to generate random *13
-            c1.setSuits("hearts");//random method suit 
-        }
-        //step 2:take input 
         
+        //step 1: generate 7 random cards and store in an array
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card((int) (Math.random() * 14), Card.SUITS[(int) (Math.random() * 4)]);
+            magicHand[i] = c;
+        }
+
+        //step 2:take input 
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the following details to see if the card you choose "
+                + "exists among the random 7 :");
+        System.out.print("Suit: ");
+        String inpSuit = scn.nextLine().toLowerCase();
+
+        System.out.print("Rank: ");
+        String inpRank = scn.nextLine();
+
+        String userInp = inpRank + " of " + inpSuit;
+
         //step 3: match with array 
+        String output = "card not found";
+        for (Card card : magicHand) {
+            if (userInp.equals(card.toString())) {
+                output = "card is found";
+                break;
+            }
+        }
+        System.out.println(output);
     }
-    
 }
